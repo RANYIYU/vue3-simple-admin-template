0531fe6bab7a3ad2fa4d37155598ee809c557c7c
Ryy <80659524+RANYIYU@users.noreply.github.com>
2024-05-04 21:30:20 +0800

    Delete wx-dianping-admin directory

--- wx-dianping-admin/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <router-view v-slot="{ Component }">
-    <component :is="Component" />
-  </router-view>
-</template>
-
-<style scoped></style>
--- wx-dianping-admin/src/views/report/ReportComponent.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>我是报表页面测试</div>
-</template>
-
-<style scoped></style>
